4e10d42bf8ffa1cd3eef2142f6b28eb0f05c23c2
Jimmyzx <125935094+Jimmyzx@users.noreply.github.com>
2023-08-14 14:46:06 -0700

    updated changes

---
+++ src/Box.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Box.css';
+
+const Box = ({ width, height, backgroundColor, removeBox }) => {
+  const boxStyle = {
+    width: `${width}px`,
+    height: `${height}px`,
+    backgroundColor,
+  };
+
+  return (
+    <div className="Box">
+      <div className="Box-box" style={boxStyle}></div>
+      <button className="Box-remove" onClick={removeBox}>
+        X
+      </button>
+    </div>
+  );
+};
+
+export default Box;
